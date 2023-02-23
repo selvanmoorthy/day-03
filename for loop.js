789e937dd55652a10da792b378cf36924edeefb8
selvanmoorthy <124973954+selvanmoorthy@users.noreply.github.com>
2023-02-23 11:24:48 +0530

    remove for in loop

--- for loop.js
+++ for loop.js
@@ -3,9 +3,4 @@ var arr=[{"name":"selvan","age":"21"},{"name":"moorthy","age":"21"}];
  
 for(i=0;i<arr.length;i++){
   console.log(arr[i].name,arr[i].age);
-
-}
-// IN FOR IN LOOP
-for(a in arr[i]){
-    console.log(a,arr[i][a]);
 }
